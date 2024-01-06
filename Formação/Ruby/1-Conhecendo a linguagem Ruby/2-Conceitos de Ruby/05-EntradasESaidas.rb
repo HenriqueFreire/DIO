ca17c9f46baf4268643828053d0a3b2928f027e6
HenriqueFreire <henriquefreire@outlook.com.br>
2024-01-06 01:52:58 -0300

    Desafio concluído

---
+++ Formação/Ruby/1-Conhecendo a linguagem Ruby/2-Conceitos de Ruby/05-EntradasESaidas.rb
@@ -0,0 +1,11 @@
+# 1-Saída de dados na tela > aparecer um comando na tela 
+# usuário digite o nome
+print "Digite seu nome: "
+
+# 2-Programa permita que o usuário preencha o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+# 3-Saída final
+# para chamar um dado #{}
+puts "Oi #{nome}!"
---
+++ Formação/Ruby/1-Conhecendo a linguagem Ruby/3-Desafio de Projeto/primeiroProjeto.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "#{nome.capitalize} #{sobrenome.capitalize} você tem #{idade} anos de idade e seu cadastro foi realizado com sucesso."

79b624b1c7c06447e12a13d4cfcfacc849777b00
HenriqueFreire <henriquefreire@outlook.com.br>
2024-02-15 13:19:33 -0300

    Métodos e gems - Usando gems

--- Formação/Ruby/3-Métodos e gems/2-usando_gems.rb
+++ Formação/Ruby/3-Métodos e gems/2-usando_gems.rb
@@ -9,9 +9,29 @@
 # a biblioteca que usaremos nessa aula será a os
 
 # 1. instalar a biblioteca (gem)
+require 'os'
 
 # Escrever nosso código 
 # iremos codar um programa que retorna algumas das configurações do nosso computador
 # para isso:
 
 # 2. definir um método
+# esse método vai analisar qual o sistema operacioal do nosso computador
+# dentro desse método, nosso código precisa analisar SE(IF) nosso sistema operacional é x
+# SeNÂO(ELSIF) y, SENÃO(ELSIF) z, SENÃO(ELSE) outra coisa não esqueça de finalizar tanto o IF quanto o DEF
+def meu_SO
+  if OS.windows?
+    "Windows"
+  elsif OS.linux?
+    "Linux"
+  elsif OS.mac?
+    "Mac"
+  else
+    "Não identifiquei o sistema operacional"
+  end
+end
+
+# 3. No final de tudo queremos que o programa imprima qual o 
+# sistema operacional e o número de bits e as cores do seu PC
+
+puts "Meu PC é #{OS.bits} bits, possui #{OS.cpu_count} cores e o sistema operacional é #{meu_SO}"

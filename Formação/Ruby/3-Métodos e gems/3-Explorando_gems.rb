fafda017fe0e9605c465c6a1f9f5ab734bb9f702
HenriqueFreire <henriquefreire@outlook.com.br>
2024-02-15 21:40:48 -0300

    Métodos e gems - Explorando gems

---
+++ Formação/Ruby/3-Métodos e gems/3-Explorando_gems.rb
@@ -0,0 +1,11 @@
+# Para instala mais de uma Gem ao Mesmo tempo usamos:
+# bundler
+
+# Passo a passo:
+# 1. Criar uma pasta nova, ex: bundler
+# 2. instalar o bundler na nossa máquina
+# para issoa gem install bundler
+# Dentro dessa pasta bundler vamos criar um aquivo chamado GemFile e vamos colocar:
+# source 'https://rubygems.org'
+# gem 'os'
+# gem (vamos pegar em um site) 

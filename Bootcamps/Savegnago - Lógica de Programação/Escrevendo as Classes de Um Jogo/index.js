ab808bb9fe6f9507e03dc12149b42f3c755e9967
HenriqueFreire <henriquefreire@outlook.com.br>
2025-06-27 01:45:52 -0300

    Bootcamp Savegnago - Lógica de Programação: Escrevendo as Classes de Um Jogo

---
+++ Bootcamps/Savegnago - Lógica de Programação/Escrevendo as Classes de Um Jogo/index.js
@@ -0,0 +1,42 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    switch (this.tipo) {
+      case 'mago':
+        ataque = 'usou magia';
+        break;
+      case 'guerreiro':
+        ataque = 'usou espada';
+        break;
+      case 'monge':
+        ataque = 'usou artes marciais';
+        break;
+      case 'ninja':
+        ataque = 'usou shuriken';
+        break;
+      default:
+        ataque = 'realizou um ataque desconhecido';
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const mago = new Heroi('Gandalf', 600, 'mago');
+mago.atacar();
+
+const guerreiro = new Heroi('Conan', 30, 'guerreiro');
+guerreiro.atacar();
+
+const ninja = new Heroi('Naruto Uzumaki', 32, 'ninja');
+ninja.atacar();
+
+const monge = new Heroi('Shaolin', 25, 'monge');
+monge.atacar();
+
+
